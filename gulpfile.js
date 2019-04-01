f56eb8ed6fe7c18129ca75a6b50efdf0c3afef15
Dekker <alx-crow@yandex.ru>
2019-04-01 22:21:18 +0300

    'add_svg'

--- gulpfile.js
+++ gulpfile.js
@@ -81,11 +81,46 @@ gulp.task('browserSync' , function(){
 // });   
 
 
+var svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace');
+
+gulp.task('svg', function(){
+  return gulp.src(baseFolder+'/img/general/svg/*.svg')
+  .pipe(svgmin({
+    js2svg: {
+      pretty: true
+    }
+  }))
+  .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {xmlMode: true}
+  }))
+  .pipe(replace('&gt;', '>'))
+  .pipe(svgSprite({
+    mode: {
+        symbol: {
+          sprite: "sprite.svg"
+        }
+    }
+  }))
+  .pipe(gulp.dest(baseFolder+'/img/general/svg/'))
+});
+
+
+
 gulp.task('watch', function(){
   gulp.watch(baseFolder+'/sass/**/*.sass', gulp.parallel('sass'));
   gulp.watch([baseFolder+'/js/**/*.js', '!'+baseFolder+'/js/*.min.js'], gulp.parallel('js'));
   gulp.watch('pug/**/*.pug', gulp.parallel('pug'));
+  gulp.watch(baseFolder+'/img/general/svg/*.svg', gulp.parallel('svg'));
 });
-gulp.task('default', gulp.parallel('watch', 'pug', 'sass', 'js', 'browserSync'));
+gulp.task('default', gulp.parallel('watch', 'pug', 'sass', 'js', 'svg', 'browserSync'));
+
 
 
